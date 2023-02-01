e2d645598c9bd4e50ca107c76cd760df23abb7ab
youngcheolLee <zodiac0905@gmail.com>
2023-02-02 01:40:14 +0900

    Update HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,29 @@
 import axios from "axios";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 export const HomePage = () => {
-  const [loaData, setLoaData] = useState<any>([]);
+  const [characterName, setCharacterName] = useState<any>("");
+  const [newsData, setNewsData] = useState<any>([]);
+
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    return setCharacterName(e.target.value);
+  };
+
+  const handleSearch = async () => {
+    const searchResponse = await axios.get(
+      `https://developer-lostark.game.onstove.com/characters/${characterName}/siblings`,
+      {
+        headers: {
+          accept: "application/json",
+          authorization: process.env.REACT_APP_LOSTARK_APIKEY,
+        },
+      }
+    );
+    console.log(searchResponse);
+  };
+
   const handleOnClick = async () => {
-    const response = await axios.get(
+    const newsResponse = await axios.get(
       "https://developer-lostark.game.onstove.com/news/events",
       {
         headers: {
@@ -13,20 +32,26 @@ export const HomePage = () => {
         },
       }
     );
-    console.log(response);
+    console.log(newsResponse);
 
-    const lostarkData = response.data;
-    setLoaData(lostarkData);
+    const lostarkData = newsResponse.data;
+    setNewsData(lostarkData);
   };
 
   return (
     <div>
-      <h1>LostArk Viewer!</h1>
-      <button onClick={handleOnClick}>getData!</button>
+      <h1>LostArk sample API</h1>
+      <div className="search">
+        ID Search :
+        <input value={characterName} onChange={handleSearchChange} />
+        <button onClick={handleSearch}>검색</button>
+      </div>
+      <div className="header">
+        <button onClick={handleOnClick}>업데이트 소식</button>
+      </div>
       <hr />
-      <h3>LostArk News</h3>
       <div className="newsList">
-        {loaData.map((item: any, idx: any) => (
+        {newsData.map((item: any, idx: any) => (
           <div key={idx} className="newsItem">
             <h4>{item.Title}</h4>
             <a target="_blank" rel="noreferrer" href={item.Link}>
