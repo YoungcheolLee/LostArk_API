ebabad68e95ea0a3cdd448de69234d108d9430a0
youngcheolLee <zodiac0905@gmail.com>
2023-02-16 16:17:14 +0900

    Update CharacterInfoPage

--- src/pages/CharacterInfoPage.tsx
+++ src/pages/CharacterInfoPage.tsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 import { CharacterDetailInfo } from "../interfaces/CharacterDetailInfo";
 
 export const CharacterInfoPage = () => {
-  const [characterInfo, setCharacterInfo] = useState<CharacterDetailInfo[]>([]);
+  const [characterInfo, setCharacterInfo] = useState<CharacterDetailInfo>();
   const { characterName } = useParams<string>();
 
   const characterDetailInfo = async () => {
@@ -26,11 +26,67 @@ export const CharacterInfoPage = () => {
 
   useEffect(() => {
     characterDetailInfo();
-  });
+  }, []);
 
   return (
-    <div>
-      <span>{characterName} deatil infomation page</span>
+    <div className="character-detailInfo-wrapper">
+      <div className="left-sort">
+        <table className="character-detailInfoTable">
+          <thead>
+            <tr>
+              <th>서버</th>
+              <td>{characterInfo?.ServerName}</td>
+            </tr>
+            <tr>
+              <th>직업 </th>
+              <td>{characterInfo?.CharacterClassName}</td>
+            </tr>
+            <tr>
+              <th>아이디</th>
+              <td>{characterInfo?.CharacterName}</td>
+            </tr>
+            <tr>
+              <th>원정대레벨</th>
+              <td>{characterInfo?.ExpeditionLevel}</td>
+            </tr>
+            <tr>
+              <th>전투레벨 </th>
+              <td>{characterInfo?.CharacterLevel}</td>
+            </tr>
+            <tr>
+              <th>아이템레벨</th>
+              <td>{characterInfo?.ItemAvgLevel}</td>
+            </tr>
+            <tr>
+              <th>길드</th>
+              <td>{characterInfo?.GuildName}</td>
+            </tr>
+            <tr>
+              <th>영지</th>
+              <td>
+                {characterInfo?.TownName} [ {characterInfo?.TownLevel}레벨 ]
+              </td>
+            </tr>
+            <tr>
+              <th>PVP</th>
+              <td>{characterInfo?.PvpGradeName}</td>
+            </tr>
+          </thead>
+        </table>
+      </div>
+
+      <div className="right-sort">
+        <table>
+          <tr>
+            <td>
+              <img
+                src={characterInfo?.CharacterImage}
+                alt="characterDetailInfo"
+              />
+            </td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 };
